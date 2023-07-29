156648c3106e9afea847462619a989c3ad77fbfa
Filip Wydra <wydra.fw@pg.com>
2023-07-29 21:13:39 +0200

    add product page

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -2,11 +2,13 @@ import { createRoutesFromElements, Route } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import HomePage from "./HomePage";
+import ProductPage from "./ProductPage";
 
 export const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
     </Route>
   )
 );
